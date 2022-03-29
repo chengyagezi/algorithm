ac3b274e256c9d3a546c6e1a1209a3947e71a15f
zhongmingjun <zhongmingjun@richinfo.cn>
2022-03-30 00:11:25 +0800

    1.两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,14 +10,21 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
+var twoSum = function (nums, target) {
+  /* for(let i=0;i<nums.length;i++){
         for(let j=i+1;j<nums.length;j++){
             if(nums[i]+nums[j]==target){
                 return [i,j];
             }
         }
+    } */
+  let isCheck = {};
+  for (let i = 0; i < nums.length; i++) {
+    let temp = target - nums[i];
+    if (temp in isCheck) {
+      return [isCheck[temp], i];
     }
+    isCheck[nums[i]] = i;
+  }
 };
 // @lc code=end
-
