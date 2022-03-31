e4c97bef58a43cf28dbaa29453db984018972e12
zhongmingjun <1214120051@qq.com>
2022-03-31 11:05:18 +0800

    300最长递增子序列

---
+++ 300.最长递增子序列.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  let length = nums.length;
+  let dp = Array(length).fill(1);
+  // 假如比前面的大，那就比前面的最长递增子序列大一
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
+};
+// @lc code=end
