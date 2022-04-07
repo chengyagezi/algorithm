5239d8d9f05df96da7e52676e91e1f84f07d66ac
zhongmingjun <1214120051@qq.com>
2022-04-07 13:25:37 +0800

    136.只出现一次的数字

---
+++ 136.只出现一次的数字.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  return nums.reduce((num, total) => num ^ total);
+};
+// @lc code=end
