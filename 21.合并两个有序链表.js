f18345ace77eaa65a02b6c14c7e1d678f1a7bb0f
zhongmingjun <zhongmingjun@richinfo.cn>
2022-04-27 22:25:38 +0800

    21合并两个有序节点

---
+++ 21.合并两个有序链表.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  // 哨兵节点  记录头节点
+  let dummy = { next: null };
+
+  let tmp = dummy;
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      dummy.next = list1;
+      list1 = list1.next;
+    } else {
+      dummy.next = list2;
+      list2 = list2.next;
+    }
+    dummy=dummy.next
+  }
+  dummy.next = list1 == null ? list2 : list1;
+  return tmp.next;
+};
+// @lc code=end
