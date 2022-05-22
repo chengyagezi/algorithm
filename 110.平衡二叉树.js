746446254883d3dae00b15e4c4448848c6e0666b
zhongmingjun <zhongmingjun@richinfo.cn>
2022-05-22 22:21:58 +0800

    101.平衡二叉树

---
+++ 110.平衡二叉树.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  function travese(node) {
+    if (node == null) return 0;
+    let left = travese(node.left);
+    let right = travese(node.right);
+    if (Math.abs(left - right) > 1 || left == -1 || right == -1) return -1;
+    return Math.max(left, right) + 1;
+  }
+  return travese(root) !== -1;
+};
+// @lc code=end
